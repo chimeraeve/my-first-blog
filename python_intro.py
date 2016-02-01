592babff46694f7133e26281f743df314a030142
Jayna Tan <jayna_tan@yahoo.com>
2016-02-02 01:14:35 +0800

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,19 @@
+print('Hello, Django girls')
+if 3 > 2:
+  print('It works!')
+
+def hi(name):
+	if name == "Ola":
+	  print("Hey Ola!")
+	elif name == "Sonia":
+	  print("Hey Sonia!")
+	else:
+	  print("Hey anon!")
+
+hi("Sally")
+
+def hi():
+	print('Hi there!')
+	print('How are you?')
+
+hi()
